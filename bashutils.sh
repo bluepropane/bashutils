6700e2144f45399f9d22acabd0f9d573d0175831
Liwei <liwei.ong@shopee.com>
2019-03-20 15:01:51 +0800

    Better handling for gitmergefrom

--- bashutils.sh
+++ bashutils.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# utils for this util
+
+# MISC
+
+alias ip="ipconfig getifaddr en0"
+# Above is for mac only. More general method is: 
+# alias ip="ifconfig en0| sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
+# https://stackoverflow.com/questions/13322485/how-to-get-the-primary-ip-address-of-the-local-machine-on-linux-and-os-x
+
 # git
 
 gitcontainscommit() {
@@ -18,9 +28,14 @@ gpush() {
 }
 
 gitmergefrom () {
-  CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-  gco $1 && git pull
-  gco $CURRENT_BRANCH && git merge $1
+  if [ "$1" != "" ]; then
+    CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+    gco $1 && git pull
+    gco $CURRENT_BRANCH && git merge $1
+  else
+    echo "Please specify a target branch to merge from."
+    echo "gitmergefrom <target_branch_name>"
+  fi
 }
 __git_complete gitmergefrom _git_checkout
 
